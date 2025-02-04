cbe1088885646210d2ceb30a74fb04a9b60f1465
BIKASH-NEPALI <nepalibikash225@gmail.com>
2025-02-04 22:46:59 +0545

    this is a copy code

---
+++ chapter4/for_loop.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+int n=8;
+for(int i=0; i<n;i++){ // initialize,test,increment
+printf("the value of i is %d", i);
+}
+return 0;
+}
---
+++ chapter4/quick_quiz2.c
@@ -0,0 +1,13 @@
+// print 'n' natural number using for loop
+#include<stdio.h>
+int main() {
+    int n=6;
+
+    for ( int i = 1; i <= n; i++)
+    {
+      printf("The value of i is %d \n", i);
+    }
+    
+
+    return 0;
+}
